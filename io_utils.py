ba243976724c6277cf33d89bf4c8a286f8cdb234
Ferran Pérez Gamonal <fperez.gamonal@gmail.com>
2019-07-24 17:15:14 +0200

    debug: hstack instead of concatenate(II)

--- io_utils.py
+++ io_utils.py
@@ -151,7 +151,7 @@ def load_matching_file(filename, width, height):
         print('empty file: %s' % filename)
     else:
         x1, y1, x2, y2 = np.loadtxt(filename, dtype=np.float32, delimiter=' ', unpack=True, usecols=(0, 1, 2, 3))
-        joint_cols = np.hstack((x1, y1, x2, y2), axis=1)
+        joint_cols = np.hstack((x1, y1, x2, y2))
         np.savetxt('joined_matches.txt', joint_cols, delimiter=' ', fmt='%.4f')
         # print("{}, {}, {}, {}".format(x1, y1, x2, y2))
         # img[np.array(y1, dtype=int), np.array(x1, dtype=int), :] = np.stack((x2 - x1, y2 - y1), axis=1)
