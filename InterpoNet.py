fix: not properly parsing args.occ_mask nor args.inv_mask in single-example inference (+add normalising constant max_flow)

InterpoNet.py
@@ -105,16 +105,26 @@ def test_one_image(args):
             # Read outputted flow to compute metrics
             pred_flow = io_utils.read_flow(out_flo_path)
 
+            if args.compute_metrics and args.gt_flow is not None:
+                gt_flow = io_utils.read_flow(args.gt_flow)
+                max_flow = np.max(np.sqrt(gt_flow[:, :, 0] ** 2 + gt_flow[:, :, 1] ** 2))  # max velocity for gt
+            else:
+                max_flow = None
             # Save image visualization of predicted flow (Middlebury colour coding)
             if args.save_image:
-                flow_img = utils.flow_to_image(pred_flow)
+                flow_img = utils.flow_to_image(pred_flow, maxflow=max_flow)
                 out_path_full = os.path.join(parent_folder_name, unique_name + '_viz.png')
                 sk.io.imsave(out_path_full, flow_img)
 
+            if args.compute_metrics and gt_flow is not None:
+                if args.occ_mask is not None:
+                    occ_mask = sk.io.imread(args.occ_mask)
+                if args.inv_mask is not None:
+                    inv_mask = sk.io.imread(args.inv_mask)
+
             # Compute metrics
             if args.compute_metrics and args.gt_flow is not None:
-                metrics = utils.compute_all_metrics(pred_flow, args.gt_flow, occ_mask=args.occ_mask,
-                                                    inv_mask=args.inv_mask)
+                metrics = utils.compute_all_metrics(pred_flow, gt_flow, occ_mask=occ_mask, inv_mask=inv_mask)
                 metrics_str = utils.get_metrics(metrics, flow_fname=unique_name)
                 print(metrics_str)  # print to stdout (use test_batch to log several images' error metrics to file)
 

io_utils.py
@@ -39,8 +39,8 @@ def load_flow_file(filename):
 
     with open(filename, 'rb') as f:
         tag = np.fromfile(f, np.float32, count=1)
-        width = np.fromfile(f, np.int32, count=1)
-        height = np.fromfile(f, np.int32, count=1)
+        width = np.fromfile(f, np.int32, count=1)[0]
+        height = np.fromfile(f, np.int32, count=1)[0]
 
         # sanity check
 
@@ -171,8 +171,8 @@ def load_edges_file(edges_file_name, width, height):
 
 
 def load_matching_file(filename, width, height):
-    img = np.zeros([height, width, 2])
-    mask = -np.ones([height, width])
+    img = np.zeros([height, width, 2]).astype(np.float32)
+    mask = -np.ones([height, width]).astype(np.uint8)
 
     if os.path.getsize(filename) == 0:
         print('empty file: %s' % filename)

utils.py
@@ -372,7 +372,7 @@ def flow_error_mask(tu, tv, u, v, mask=None, gt_value=False, bord=0):
     return mang, stdang, mepe
 
 
-def flow_to_image(flow):
+def flow_to_image(flow, maxflow=None):
     """
     Convert flow into middlebury color code image
     :param flow: optical flow map
@@ -397,7 +397,10 @@ def flow_to_image(flow):
     minv = min(minv, np.min(v))
 
     rad = np.sqrt(u ** 2 + v ** 2)
-    maxrad = max(-1, np.max(rad))
+    if maxflow is None:
+        maxrad = max(-1, np.max(rad))
+    else:
+        maxrad = maxflow
 
     if DEBUG:
         print("max flow: {.4f}\nflow range:\nu = {.3f} .. {.3f}\nv = {.3f} .. {.3f}".format(maxrad, minu, maxu, minv,