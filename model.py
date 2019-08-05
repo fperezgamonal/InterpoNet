fb52b68abef5abf0a73bc30e4ef24deaa081b599
Ferran Pérez Gamonal <fperez.gamonal@gmail.com>
2019-08-05 11:44:51 +0200

    do not have compat w. TF2.0 'bug-free' resize, use TF1 instead...

--- model.py
+++ model.py
@@ -44,8 +44,8 @@ def getNetwork(input_img, mask, edges, og_height, og_width, reuse=tf.AUTO_REUSE)
     # Desireable
     # flow = tf_v2.image.resize(detour10, tf.stack([og_height, og_width]), method=ResizeMethod.BILINEAR,
     #                          preserve_aspect_ratio=True)
-    flow = tf.image.resize_bicubic(detour10, tf.stack([og_height, og_width]), align_corners=True,
-                                   half_pixel_centers=True)
+    flow = tf.image.resize_bicubic(detour10, tf.stack([og_height, og_width]), align_corners=True,)
+    # half_pixel_centers=True) not available on 1.12 (cluster version) :(
 
     return flow
 
